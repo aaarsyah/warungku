aa6196a16d428c5c075d522adbafd8464fab518b
Addinda Ayu Arsyah <addinda.arsyah21@mhs.uinjkt.ac.id>
2024-09-30 23:19:12 +0700

    first commit

---
+++ dashboard.php
@@ -0,0 +1,49 @@
+<?php
+    session_start();
+    if($_SESSION['status_login'] != true){
+        echo '<script>window.location="login.php"</script>';
+    }
+?>
+
+<!DOCTYPE html> 
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>
+            Warungku
+        </title>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <div class="container">
+            <a href="dashboard.php"><img src="img/icon-warungku.png" width="60px"></a>    
+            <h1><a href="dashboard.php">Warungku</a></h1>
+            <ul>
+                <li><a href="dashboard.php">Dashboard</a></li>
+                <li><a href="profil.php">Profil</a></li>
+                <li><a href="data-kategori.php">Data Kategori</a></li>
+                <li><a href="data-produk.php">Data Produk</a></li>
+                <li><a href="logout.php">Logout</a></li>
+            </ul>
+            </div>
+        </header>
+        <div class="section">
+            <div class="container">
+                <h3>Dashboard</h3>
+                <div class="box">
+                    <h4>Selamat Datang <?php echo $_SESSION['a_global']->admin_name ?> !</h4>
+                </div>
+            </div>
+        </div>
+        <footer>
+            <div class="container">
+                <small>
+                    Copyright &copy; 2022 - Warungku
+                </small>
+            </div>
+        </footer>
+    </body>
+</html>
